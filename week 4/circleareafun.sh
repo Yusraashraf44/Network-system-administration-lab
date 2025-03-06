e3b6307a69dd1656b8a31f6d2c5e24807939f0d0
YUSRA <yusraash44@gmail.com>
2025-03-06 11:27:20 +0530

    Add files via upload

---
+++ week 4/circleareafun.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+calculate_area() {
+    radius=$1
+    
+    area=$(echo "scale=2; 3.14159 * $radius * $radius" | bc)
+    echo "The area of the circle with radius $radius is: $area"
+}
+
+echo "Enter the radius of the circle:"
+read radius
+
+# Call the calculate_area function 
+calculate_area $radius
+
