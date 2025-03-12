ec19c31e5c5b1b540af351a2736141fcaa32c004
YUSRA <yusraash44@gmail.com>
2025-03-12 13:38:41 +0530

    Add files via upload

--- week 4/displayelements.sh
+++ week 4/displayelements.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
-
-# Function to input elements into the array
-input_array() {
-    echo "Enter the number of elements in the array:"
-    read n
-    arr=()
-    for (( i=0; i<n; i++ ))
-    do
-        echo "Enter element $((i+1)):"
-        read element
-        arr+=("$element")  # Add element to array
-    done
-}
-
-# Function to display the elements of the array
-display_array() {
-    echo "The elements in the array are:"
-    for element in "${arr[@]}"
-    do
-        echo "$element"
-    done
-}
-
-# Call the functions
-input_array
-display_array
-
+echo "Enter no of elements in array: ";
+read n;
+declare -a arr
+for ((i=0;i<n;i++))
+do 
+	echo "Enter element $((i+1)) of array: ";
+	read arr[$i];
+done
+echo "Elements of the array are: "
+for ((i=0;i<n;i++))
+do 
+	echo "${arr[$i]}";
+done
